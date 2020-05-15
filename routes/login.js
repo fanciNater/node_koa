636f39ae8ef0e03ba9c331210b1f4efe5dccea3d
吴灿 <wucan@wucandeMacBook-Pro.local>
2020-05-15 16:37:25 +0800

    write something

--- routes/login.js
+++ routes/login.js
@@ -2,19 +2,18 @@ const router = require("koa-router")();
 const LoginServie = require("../services/login.service");
 const RespJson = require("../model/RespJson");
 
-router.post("/login", async (req, res) => {
+router.post("/login", async (ctx, next) => {
     let loginService = new LoginServie();
-    let params = req.request.body;
-    let result = await loginService.checkUser(params);
+    let params = ctx.request.body;
     try {
+        let result = await loginService.checkUser(params);
         if (result.length === 1) {
-            // res.json(new RespJson("ok", "登录成功"));
-            req.body = new RespJson("ok", "登录成功");
+            ctx.body = new RespJson("ok", "登录成功");
         } else {
-            // res.json(new RespJson("error", "账号或密码错误，请重试"));
+            ctx.body = new RespJson("error", "账号或密码错误，请重试");
         }
     } catch (error) {
-        res.json(new RespJson("error", "服务器错误"));
+        ctx.body = new RespJson("error", "服务器错误");
     }
 });
 
