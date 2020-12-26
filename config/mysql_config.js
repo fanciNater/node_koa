2f8dd8c95409bc81cf82ca4636f9f0cab0b81a36
吴灿 <wucan@wucandeMacBook-Pro.local>
2020-12-26 15:36:10 +0800

    mysql-config

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-18 18:25:22
+ * @LastEditTime: 2020-12-26 15:35:52
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /vsCodeProjects/demo/nodeJs/node_koa/config/mysql_config.js
+ */
 const mysql = require("mysql");
 
 class mysqlConfig {
@@ -6,7 +14,7 @@ class mysqlConfig {
             user: "root",
             password: "123456",
             database: "demo",
-            host: "47.114.52.172",
+            host: "127.0.0.1",
             port: "3306"
         });
         conn.connect();
